70bcafc769c05b4c407e9ab60641d9c5eec6dde2
bhuwansh <bsbsharma47@gmail.com>
2021-04-03 12:16:19 +0530

    apply code

--- Main.py
+++ Main.py
@@ -1,2 +1,13 @@
 
-From Pytube import YouTube . 
+from Pytube import YouTube
+link = input("Enter URL of video")
+video = YouTube(link)
+stream = video.streams.get_highest_resolution()
+stream.download() 
+
+
+
+
+#output:   
+#    from Pytube import YouTube
+#    ModuleNotFoundError: No module named 'Pytube'
